93c41789aa63f5ac34ffa8f9ea43e5d5a3b84113
woomj <speedcross99@naver.com>
2020-06-10 12:13:45 +0900

    profile/counter/todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import * as React from "react";
 import Profile from "./components/Profile";
+import Counter from "./components/Counter";
+import TodoList from "./components/TodoList";
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Profile name="우민제" job="코드사망꾼" />
+        <Counter />
+        <TodoList />
       </div>
     );
   }
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -5,22 +5,18 @@ interface Props {
   job: string;
 }
 
-class Profile extends React.Component<Props> {
-  render() {
-    const { name, job } = this.props;
-    return (
-      <div>
-        <h1>프로필</h1>
-        <div>
-          <b>이름: </b>
-          {name}
-        </div>
-        <div>
-          <b>직업: </b>
-          {job}
-        </div>
-      </div>
-    );
-  }
-}
+const Profile: React.SFC<Props> = ({ name, job }: Props) => (
+  <div>
+    <h1>프로필</h1>
+    <div>
+      <b>이름: </b>
+      {name}
+    </div>
+    <div>
+      <b>직업: </b>
+      {job}
+    </div>
+  </div>
+);
+
 export default Profile;
